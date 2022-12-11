43f3e6313147764fed3ee0f29014365c652b5ba2
cjh1830 <113440277+cjh1830@users.noreply.github.com>
2022-12-11 23:32:25 +0900

    Add files via upload

---
+++ recommend_music_service.py
@@ -0,0 +1,111 @@
+music_list = [{'singer':'BTS', 'title':'Dynamite', 'genre':'dance', 'gender':'m', 'mood':'exciting', 'length':3, 'tempo':3 },
+              {'singer':'창모' , 'title':'METEOR' , 'genre':'hiphop' , 'gender':'m' , 'mood':'refined' , 'length':3 , 'tempo':2 },
+              {'singer':'버스커버스커' , 'title':'벚꽃 엔딩' , 'genre':'rock' , 'gender':'m' , 'mood':'exciting' , 'length':4 , 'tempo':3 },
+              {'singer':'아이유' , 'title':'밤편지' , 'genre':'ballad' , 'gender':'w' , 'mood':'calm' , 'length':4 , 'tempo':1 },
+              {'singer':'박효신' , 'title':'야생화' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed', 'length':5 , 'tempo':1 },
+              {'singer':'나얼' , 'title':'바람기억' , 'genre':'ballad' , 'gender':'m' , 'mood':'sad' , 'length':5 , 'tempo':1 },
+              {'singer':'뉴진스' , 'title':'hype boy' , 'genre':'dance' , 'gender':'w' , 'mood':'refined' , 'length':3 , 'tempo':2 },
+              {'singer':'IVE' , 'title':'after like' , 'genre':'dance' , 'gender':'w' , 'mood':'exciting' , 'length':2 , 'tempo':3 },
+              {'singer':'이무진' , 'title':'신호등' , 'genre':'rock' , 'gender':'m' , 'mood':'light' , 'length':3 , 'tempo':2 },
+              {'singer':'aespa' , 'title':'Next Level' , 'genre':'dance' , 'gender':'w' , 'mood':'refined' , 'length':3 , 'tempo':2 },
+              {'singer':'sokodomo' , 'title':'회전목마' , 'genre':'hiphop' , 'gender':'m' , 'mood':'light' , 'length':4 , 'tempo':2 },
+              {'singer':'윤하' , 'title':'사건의 지평선' , 'genre':'rock' , 'gender':'w' , 'mood':'overflowing' , 'length':5 , 'tempo':2 },
+              {'singer':'윤종신' , 'title':'좋니' , 'genre':'ballad' , 'gender':'m' , 'mood':'sad' , 'length':5 , 'tempo':1 },
+              {'singer':'MC the max' , 'title':'어디에도' , 'genre':'ballad' , 'gender':'m' , 'mood':'sad' , 'length':5 , 'tempo':1 },
+              {'singer':'폴킴' , 'title':'모든 날, 모든 순간' , 'genre':'ballad' , 'gender':'m' , 'mood':'calm' , 'length':3 , 'tempo':1 },
+              {'singer':'ash island' , 'title':'paranoid' , 'genre':'hiphop' , 'gender':'m' , 'mood':'overflowing' , 'length':3 , 'tempo':2 },
+              {'singer':'에일리' , 'title':'첫눈처럼 너에게 가겠다' , 'genre':'ballad' , 'gender':'w' , 'mood':'sad' , 'length':3 , 'tempo':1 },
+              {'singer':'호미들' , 'title':'사이렌' , 'genre':'hiphop' , 'gender':'m' , 'mood':'impressed' , 'length':3 , 'tempo':3 },
+              {'singer':'부활' , 'title':'never ending story' , 'genre':'rock' , 'gender':'m' , 'mood':'overflowing' , 'length':4 , 'tempo':1 },
+              {'singer':'성시경' , 'title':'거리에서' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed' , 'length':4 , 'tempo':1 },
+              {'singer':'빅뱅' , 'title':'봄여름가을겨울' , 'genre':'rock' , 'gender':'m' , 'mood':'impressed' , 'length':3 , 'tempo':1 },
+              {'singer':'헤이즈' , 'title':'비도 오고 그래서' , 'genre':'ballad' , 'gender':'w' , 'mood':'dark' , 'length':3 , 'tempo':2 },
+              {'singer':'볼빨간사춘기' , 'title':'우주를 줄게' , 'genre':'rock' , 'gender':'w' , 'mood':'light' , 'length':3 , 'tempo':2 },
+              {'singer':'데이식스' , 'title':'예뻤어' , 'genre':'rock' , 'gender':'m' , 'mood':'overflowing' , 'length':4 , 'tempo':3 },
+              {'singer':'비와이' , 'title':'forever' , 'genre':'hiphop' , 'gender':'m' , 'mood':'holy' , 'length':4 , 'tempo':2 },
+              {'singer':'트와이스' , 'title':'cheer up' , 'genre':'dance' , 'gender':'w' , 'mood':'exciting' , 'length':3 , 'tempo':3 },
+              {'singer':'빅뱅' , 'title':'뱅뱅뱅' , 'genre':'dance' , 'gender':'m' , 'mood':'exciting' , 'length':3 , 'tempo':3 },
+              {'singer':'넬' , 'title':'기억을 걷는 시간' , 'genre':'rock' , 'gender':'m' , 'mood':'impressed' , 'length':5 , 'tempo':2 },
+              {'singer':'이하이' , 'title':'한숨' , 'genre':'ballad' , 'gender':'w' , 'mood':'sad' , 'length':4 , 'tempo':1 },
+              {'singer':'블루' , 'title':'downtown baby' , 'genre':'hiphop' , 'gender':'m' , 'mood':'dark' , 'length':3 , 'tempo':3 },
+              {'singer':'김민석' , 'title':'취중고백' , 'genre':'ballad' , 'gender':'m' , 'mood':'calm' , 'length':3 , 'tempo':1 },
+              {'singer':'아이유' , 'title':'Blueming' , 'genre':'rock' , 'gender':'w' , 'mood':'light' , 'length':3 , 'tempo':2 },
+              {'singer':'blackpink' , 'title':'lovesick girls' , 'genre':'dance' , 'gender':'w' , 'mood':'refined', 'length':3 , 'tempo':3 },
+              {'singer':'장범준' , 'title':'잠이 오질 않네요' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed', 'length':4 , 'tempo':2 },
+              {'singer':'브레이브걸스' , 'title':'롤린' , 'genre':'dance' , 'gender':'w' , 'mood':'exciting', 'length':3 , 'tempo':3 },
+              {'singer':'BTS' , 'title':'작은 것들을 위한 시' , 'genre':'dance' , 'gender':'m' , 'mood':'light', 'length':3 , 'tempo':3 },
+              {'singer':'마크둡' , 'title':'오늘도 빛나는 너에게' , 'genre':'ballad' , 'gender':'m' , 'mood':'holy', 'length':4 , 'tempo':1 },
+              {'singer':'규현' , 'title':'내 마음이 움찔했던 순간' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed', 'length':3 , 'tempo':1 },
+              {'singer':'태연' , 'title':'그대라는 시' , 'genre':'ballad' , 'gender':'w' , 'mood':'calm', 'length':3 , 'tempo':1 },
+              {'singer':'잔나비' , 'title':'주저하는 연인들을 위해' , 'genre':'rock' , 'gender':'m' , 'mood':'impressed', 'length':4 , 'tempo':2 },
+              {'singer':'어쿠스틱 콜라보' , 'title':'묘해, 너와' , 'genre':'ballad' , 'gender':'w' , 'mood':'impressed', 'length':4 , 'tempo':1 },
+              {'singer':'비오' , 'title':'Counting stars' , 'genre':'hiphop' , 'gender':'m' , 'mood':'light' , 'length':2, 'tempo':2},
+              {'singer':'김필' , 'title':'다시 사랑한다면' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed' , 'length':4 , 'tempo':2},
+              {'singer':'릴러말즈' , 'title':'break up' , 'genre':'hiphop' , 'gender':'m' , 'mood':'overflowing' , 'length':2 , 'tempo':2},
+              {'singer':'디핵' , 'title':'별과 꿈의 이야기' , 'genre':'hiphop' , 'gender':'m' , 'mood':'impressed' , 'length':3 , 'tempo':2},
+              {'singer':'성시경' , 'title':'너의 모든 순간' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed' , 'length':4 , 'tempo':1},
+              {'singer':'winner' , 'title':'really really' , 'genre':'dance' , 'gender':'m' , 'mood':'refined' , 'length':3 , 'tempo':3},
+              {'singer':'여자친구' , 'title':'시간을 달려서' , 'genre':'dance' , 'gender':'w' , 'mood':'overflowing' , 'length':3 , 'tempo':2},
+              {'singer':'BTS' , 'title':'피 땀 눈물' , 'genre':'dance' , 'gender':'m' , 'mood':'dark' , 'length':3 , 'tempo':2},
+              {'singer':'아이유' , 'title':'금요일에 만나요' , 'genre':'ballad' , 'gender':'w' , 'mood':'light' , 'length':3 , 'tempo':2},
+              {'singer':'빅뱅' , 'title':'하루 하루' , 'genre':'dance' , 'gender':'m' , 'mood':'sad' , 'length':4 , 'tempo':3},
+              {'singer':'블락비' , 'title':'HER' , 'genre':'dance' , 'gender':'m' , 'mood':'exciting' , 'length':2 , 'tempo':3},
+              {'singer':'스트레이' , 'title':'너, 너' , 'genre':'ballad' , 'gender':'m' , 'mood':'impressed' , 'length':4 , 'tempo':1},
+              {'singer':'빅마마' , 'title':'체념' , 'genre':'ballad' , 'gender':'w' , 'mood':'sad' , 'length':4 , 'tempo':1},
+              {'singer':'체리필터' , 'title':'낭만 고양이' , 'genre':'rock' , 'gender':'w' , 'mood':'overflowing' , 'length':3 , 'tempo':3},
+              {'singer':'노라조' , 'title':'형' , 'genre':'rock' , 'gender':'m' , 'mood':'impressed' , 'length':4 , 'tempo':1}]     # 가수, 제목, 장르, 성별, 분위기, 길이, 빠르기,
+
+while(True):
+    genre_list, gender_list, mood_list, length_list, your_music = [], [], [], [], []
+    
+    ans = list(input("노래 추천 서비스입니다. 어떤 장르의 노래를 원하시나요?(dance, ballad, rock, hiphop, 상관없으면 엔터)\n(중복 가능 ex:dance, ballad, ...)\n").split(', '))
+    if ans == ['']:
+        genre_list = music_list
+    else:
+        for line in music_list:
+            if line.get('genre') in ans:
+                genre_list.append(line)
+                
+    ans = input("가수의 원하시는 성별을 입력해 주세요.(m, w, 상관없으면 엔터)\n")
+    if ans == "":
+        gender_list = genre_list
+    else:
+        for line in genre_list:
+            if ans == line.get('gender'):
+                gender_list.append(line)
+                
+    ans = list(input("원하는 분위기를 입력해 주세요.(exciting, dark, light, holy, impressed, overflowing, sad, refined, calm, 상관없으면 엔터)\n(중복 가능 ex:holy, light, ..)\n").split(', '))
+    if ans == ['']:
+        mood_list = gender_list
+    else:
+        for line in gender_list:
+            if line.get('mood') in ans:
+                mood_list.append(line)
+    
+    ans = list(input("원하는 노래 길이를 입력해 주세요.(2: 2:00~2:59 3: 3:00~3:59, 4: 4:00~4:59, 5: 5:00~, 상관없으면 엔터)\n(중복 가능 ex:2, 3, ..)\n").split(', '))
+    if ans == ['']:
+        length_list = mood_list
+    else:
+        for line in mood_list:
+            if str(line.get('length')) in ans:
+                length_list.append(line)
+                
+    ans = list(input("원하시는 빠르기를 입력해 주세요.(1: 느림, 2: 보통, 3: 빠름, 상관없으면 엔터\n(중복 가능 ex:1, 2)\n").split(', '))
+    if ans == ['']:
+        your_music = length_list
+    else:
+        for line in length_list:
+            if str(line.get('tempo')) in ans:
+                your_music.append(line)
+                
+    if bool(your_music) == False:
+        print("조건을 충족하는 음악이 없습니다.")
+    else:
+        print("추천해 드릴 음악은 다음과 같습니다.")
+        for line in your_music:
+            print(line.get('singer'), "-", line.get('title'))
+    
+    if input("다시 하시겠습니까? (하려면 y, 아니면 아무 키)\n") == 'y':
+        pass
+    else:
+        break
